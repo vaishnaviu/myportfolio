2aadb2d17664be797c3c1a1fcccfa750d998aef3
vaishnaviu <vaishnaviu96@gmail.com>
2024-04-02 16:50:40 -0700

    Updating experience

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ function Header() {
       <nav>
         <ul>
           <li><a href="#about">About</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
